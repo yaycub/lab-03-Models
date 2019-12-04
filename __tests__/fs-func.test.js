ba63944f40c6363580f2800b38f103b884d30073
Jacob Harrington <jharrington4@gmail.com>
2019-12-04 15:32:49 -0800

    finished readDirectoryJSON and tests passing

--- __tests__/fs-func.test.js
+++ __tests__/fs-func.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs').promises;
-const { mkdirp, writeJSON, readJSON } = require('../lib/fs-func');
+const { mkdirp, writeJSON, readJSON, readDirectoryJSON } = require('../lib/fs-func');
 
 describe('fs-functions', () => {
   beforeAll(() => {
@@ -11,7 +11,7 @@ describe('fs-functions', () => {
   });
 
   afterAll(() => {
-    return fs.rmdir('./test/');
+    return fs.rmdir('./test/', { recursive: true });
   });
 
   it('can write an object to a file', async() => {
@@ -27,5 +27,11 @@ describe('fs-functions', () => {
     const dog = { name: 'spot', age: 5 };
     const data = await readJSON('./test/test/test.txt', 'utf8');
     expect(data).toEqual(dog);
+  });
+
+  it('can read all files in a directory as objects', async() => {
+    const data = await fs.readdir('./test/test/');
+    const result = await readDirectoryJSON('./test/test/');
+    expect(data).toEqual(result);
   });
 });
