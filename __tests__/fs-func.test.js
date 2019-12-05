d7cb3eaa0e8c6c22540568f4c9c4902db1f513f3
Jacob Harrington <jharrington4@gmail.com>
2019-12-04 16:06:27 -0800

    started updateJSON but can not pass tests. Probably wont be touching this lab after this commit. byeeeeee

--- __tests__/fs-func.test.js
+++ __tests__/fs-func.test.js
@@ -1,14 +1,14 @@
 const fs = require('fs').promises;
-const { mkdirp, writeJSON, readJSON, readDirectoryJSON } = require('../lib/fs-func');
+const { mkdirp, writeJSON, readJSON, readDirectoryJSON, updateJSON } = require('../lib/fs-func');
 
 describe('fs-functions', () => {
   beforeAll(() => {
     return mkdirp('./test/test/');
   });
 
-  afterAll(() => {
-    return fs.unlink('./test/test/test.txt');
-  });
+  // afterAll(() => {
+  //   return fs.unlink('./test/test/test.txt');
+  // });
 
   afterAll(() => {
     return fs.rmdir('./test/', { recursive: true });
@@ -16,8 +16,8 @@ describe('fs-functions', () => {
 
   it('can write an object to a file', async() => {
     const dog = { name: 'spot', age: 5 };
-    await writeJSON('./test/test/test.txt', dog);
-    fs.readFile('./test/test/test.txt', 'utf8')
+    await writeJSON('./test/test/test.js', dog);
+    fs.readFile('./test/test/test.js', 'utf8')
       .then(result => {
         expect(JSON.parse(result)).toEqual(dog);
       });
@@ -25,7 +25,7 @@ describe('fs-functions', () => {
 
   it('can read an object from a file', async() => {
     const dog = { name: 'spot', age: 5 };
-    const data = await readJSON('./test/test/test.txt', 'utf8');
+    const data = await readJSON('./test/test/test.js', 'utf8');
     expect(data).toEqual(dog);
   });
 
@@ -33,5 +33,12 @@ describe('fs-functions', () => {
     const data = await fs.readdir('./test/test/');
     const result = await readDirectoryJSON('./test/test/');
     expect(data).toEqual(result);
+  });
+
+  it('can update a files JSON', async() => {
+    const result = { name: 'spot', age: 5, weight: '20 lbs' };
+    await updateJSON('./test/test/test.js', { weight: '20 lbs' });
+    const file = await readJSON('./test/test/test.js');
+    expect(file).toEqual(result);
   });
 });
